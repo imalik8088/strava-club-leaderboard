37849cee96f306576ec43668b1beb9c8f0140775
Akhlaq Malik <info@imalik8088.de>
2020-12-21 22:30:18 +0100

    init evaluation of scraped data

---
+++ post_processing.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+from pydantic import BaseModel
+from tinydb import TinyDB, Query
+from tinydb.database import Table
+
+
+class User(BaseModel):
+    user_id: str
+    name: str
+    profile_url: str
+    cycling_distance_in_km: float
+    cycling_duration_in_minute: int
+    running_distance_in_km: float
+    running_duration_in_minute: int
+    swimming_distance_in_meter: float
+    swimming_duration_in_minute: int
+
+
+def _minutes_in_hours(minutes):
+    hours = int(minutes / 60)
+    minute = minutes % hours
+    return "%d hours %02d minutes" % (hours, minute)
+
+
+db: Table = TinyDB('./db/strava-leaderboard.json')
+Leaderboard = Query()
+
+members = {}
+get_all_entries = db.all()
+for entry in get_all_entries:
+    member = User(name=entry.get('name'),
+                  user_id=entry.get('user_id'),
+                  profile_url=entry.get('profile_url'),
+                  cycling_distance_in_km=entry.get('cycling_distance_in_km'),
+                  cycling_duration_in_minute=entry.get('cycling_duration_in_minute'),
+                  running_distance_in_km=entry.get('running_distance_in_km'),
+                  running_duration_in_minute=entry.get('running_duration_in_minute'),
+                  swimming_distance_in_meter=entry.get('swimming_distance_in_meter'),
+                  swimming_duration_in_minute=entry.get('swimming_duration_in_minute'),
+                  )
+    if member.user_id == "64492740":
+        continue
+    members[member.user_id] = member
+
+print(f"parsed {len(members)} members")
+
+# ----------- LEADERBOARD: active member
+class ActiveLeader(BaseModel):
+    name: str
+    duration: str
+
+class UserDuration(BaseModel):
+    user_id: str
+    name: str
+    duration: int
+
+total_member_activity_duration = []
+for user_id, m in members.items():
+    member_duration = UserDuration(user_id=m.user_id,
+                                   name=m.name,
+                                   duration=m.cycling_duration_in_minute + m.running_duration_in_minute + m.swimming_duration_in_minute)
+    total_member_activity_duration.append(member_duration)
+
+total_member_activity_duration = sorted(total_member_activity_duration, key=lambda k: k.duration, reverse=True)
+leaderboard_duration = []
+for member in total_member_activity_duration[:5]:
+    leaderboard_duration.append(ActiveLeader(
+        name=member.name,
+        duration=_minutes_in_hours(member.duration)))
+
+print(f"[🕓] Most active member: {leaderboard_duration}")
+
+
+# ----------- LEADERBOARD: cycling
+class CyclingLeader(BaseModel):
+    name: str
+    cycling_distance_in_km: float
+
+leaderboard_cycling = []
+for k, v in members.items():
+    leaderboard_cycling.append(CyclingLeader(name=v.name, cycling_distance_in_km=v.cycling_distance_in_km))
+
+cycling_distance_in_km = sorted(leaderboard_cycling, key=lambda k: k.cycling_distance_in_km, reverse=True)
+print(f"[🚴] Most active cyclists {cycling_distance_in_km[:5]}")
+
+# ----------- LEADERBOARD: running
+class RunnerLeader(BaseModel):
+    name: str
+    running_distance_in_km: float
+
+leaderboard_cycling = []
+for k, v in members.items():
+    leaderboard_cycling.append(RunnerLeader(name=v.name, running_distance_in_km=v.running_distance_in_km))
+
+running_distance_in_km = sorted(leaderboard_cycling, key=lambda k: k.running_distance_in_km, reverse=True)
+print(f"[🏃‍] Most active runner {running_distance_in_km[:5]}")
+
+# ----------- LEADERBOARD: swimming
+class SwimmingLeader(BaseModel):
+    name: str
+    swimming_distance_in_meter: float
+
+leaderboard_cycling = []
+for k, v in members.items():
+    leaderboard_cycling.append(SwimmingLeader(name=v.name, swimming_distance_in_meter=v.swimming_distance_in_meter))
+
+swimming_distance_in_meter = sorted(leaderboard_cycling, key=lambda k: k.swimming_distance_in_meter, reverse=True)
+print(f"[🏊‍] Most active swimmer {swimming_distance_in_meter[:5]}")
