b3a486432dce7daaff1689a59a9e2a3bc70c9122
Akhlaq Malik <info@imalik8088.de>
2024-01-09 11:52:37 +0100

    hm and optional fields

--- post_processing.py
+++ post_processing.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, Field
 from tinydb import TinyDB, Query
 from tinydb.database import Table
 
@@ -9,12 +11,13 @@ class User(BaseModel):
     user_id: str
     name: str
     profile_url: str
-    cycling_distance_in_km: float
-    cycling_duration_in_minute: int
-    running_distance_in_km: float
-    running_duration_in_minute: int
-    swimming_distance_in_meter: float
-    swimming_duration_in_minute: int
+    cycling_distance_in_km: Optional[float] = Field(default=0.0)
+    cycling_duration_in_minute: Optional[int] = Field(default=0)
+    cycling_elevation_in_meter: Optional[int] = Field(default=0)
+    running_distance_in_km: Optional[float] = Field(default=0.0)
+    running_duration_in_minute: Optional[int] = Field(default=0)
+    swimming_distance_in_meter: Optional[float] = Field(default=0.0)
+    swimming_duration_in_minute: Optional[int] = Field(default=0)
 
 
 def _minutes_in_hours(minutes):
@@ -23,9 +26,9 @@ def _minutes_in_hours(minutes):
     return "%d hours %02d minutes" % (hours, minute)
 
 
-TOP_X_MEBMER = 5
+TOP_X_MEBMER = 10
 
-db: Table = TinyDB('./db/strava-leaderboard-14632-2020.json')
+db: Table = TinyDB('./db/strava-leaderboard-285486-2023.json')
 Leaderboard = Query()
 
 members = {}
@@ -34,14 +37,17 @@ for entry in get_all_entries:
     member = User(name=entry.get('name'),
                   user_id=entry.get('user_id'),
                   profile_url=entry.get('profile_url'),
-                  cycling_distance_in_km=entry.get('cycling_distance_in_km'),
-                  cycling_duration_in_minute=entry.get('cycling_duration_in_minute'),
-                  running_distance_in_km=entry.get('running_distance_in_km'),
-                  running_duration_in_minute=entry.get('running_duration_in_minute'),
-                  swimming_distance_in_meter=entry.get('swimming_distance_in_meter'),
-                  swimming_duration_in_minute=entry.get('swimming_duration_in_minute'),
+                  cycling_distance_in_km=entry.get('cycling_distance_in_km', 0),
+                  cycling_elevation_in_meter=entry.get('cycling_elevation_in_meter', 0),
+                  cycling_duration_in_minute=entry.get('cycling_duration_in_minute', 0),
+                  running_distance_in_km=entry.get('running_distance_in_km', 0),
+                  running_duration_in_minute=entry.get('running_duration_in_minute', 0),
+                  swimming_distance_in_meter=entry.get('swimming_distance_in_meter', 0),
+                  swimming_duration_in_minute=entry.get('swimming_duration_in_minute', 0),
                   )
-    if member.user_id == "64492740":
+
+    exclude_users = ['25222483']
+    if member.user_id in exclude_users:
         continue
     members[member.user_id] = member
 
@@ -84,16 +90,17 @@ print()
 class CyclingLeader(BaseModel):
     name: str
     cycling_distance_in_km: float
+    cycling_elevation_in_meter: int
 
 
 leaderboard_cycling = []
 for k, v in members.items():
-    leaderboard_cycling.append(CyclingLeader(name=v.name, cycling_distance_in_km=v.cycling_distance_in_km))
+    leaderboard_cycling.append(CyclingLeader(name=v.name, cycling_distance_in_km=v.cycling_distance_in_km, cycling_elevation_in_meter=v.cycling_elevation_in_meter))
 
 cycling_distance_in_km = sorted(leaderboard_cycling, key=lambda k: k.cycling_distance_in_km, reverse=True)
 print("🚴 Most active cyclists")
 for index, member in enumerate(cycling_distance_in_km[:TOP_X_MEBMER], start=1):
-    print(f"{index}. {member.name} - {member.cycling_distance_in_km} Km")
+    print(f"{index}. {member.name} - {member.cycling_distance_in_km} Km - {member.cycling_elevation_in_meter}hm")
 print()
 
 
